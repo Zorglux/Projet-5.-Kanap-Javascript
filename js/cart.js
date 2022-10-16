3eb292633a0fed58bf620982499c8e766d28b783
Zorglux <tomasdusoulier@gmail.com>
2022-10-16 14:38:33 +0200

    actualisation.

--- js/cart.js
+++ js/cart.js
@@ -34,17 +34,17 @@ async function updatePriceQuantity() {
 // ==========================================================
 
 async function creerElements(){
-  let localStorage = getItemsFromLocalStorage()
+  let localStorageItem = getItemsFromLocalStorage()
 
   // creation des élements
-  for (let i=0; localStorage[i]; i++){
-    let donneesApi = await getDataById(localStorage[i].identifiant)
+  for (let i=0; localStorageItem[i]; i++){
+    let donneesApi = await getDataById(localStorageItem[i].identifiant)
     const placement = document.getElementById("cart__items")
 
     const article = document.createElement("article")
     article.classList.add("cart__item")
-    article.setAttribute("data-id", `${localStorage[i].identifiant}`)
-    article.setAttribute("data-color", `${localStorage[i].couleur}`)
+    article.setAttribute("data-id", `${localStorageItem[i].identifiant}`)
+    article.setAttribute("data-color", `${localStorageItem[i].couleur}`)
     article.innerHTML =
      ` <div class="cart__item__img">
      <img src="${donneesApi.imageUrl}" alt="${donneesApi.altTxt}">
@@ -52,13 +52,13 @@ async function creerElements(){
    <div class="cart__item__content">
      <div class="cart__item__content__description">
        <h2>${donneesApi.name}</h2>
-       <p>${localStorage[i].couleur}</p>
+       <p>${localStorageItem[i].couleur}</p>
        <p>${donneesApi.price}</p>
      </div>
      <div class="cart__item__content__settings">
        <div class="cart__item__content__settings__quantity">
          <p>Qté : </p>
-         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localStorage[i].montant}">
+         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localStorageItem[i].montant}">
        </div>
        <div class="cart__item__content__settings__delete">
          <p class="deleteItem">Supprimer</p>
@@ -67,6 +67,50 @@ async function creerElements(){
    </div>`
 
     placement.appendChild(article)
+
+    let qtyInput = article.getElementsByClassName('itemQuantity');
+      console.log("n'est pas un tableau?", qtyInput[0])
+      qtyInput[0].addEventListener('change', (e) => {
+        updateQtyLocalStorage(e, localStorageItem[i].id, localStorageItem[i].color);
+      });
+      
+      /**
+       * @param  {HTMLElement} q
+       * @param  {string} id
+       * @param  {string} color
+       * update localStorage when a quantity change is triggered
+       */
+      async function updateQtyLocalStorage(q, id, color) {
+        let array = getItemsFromLocalStorage();
+        let idArray = [];
+        let index;
+        // error handling
+        if (q.target.value <= 0)
+          return -1;
+        // ----- get the index of our item in our array ----- //
+        for (let i = 0; array[i]; i++) {
+          if (array[i].id == id && array[i].color == color)
+            index = i;
+          idArray.push(array[i].id);
+        }
+      
+        // ----- change the value of the quantity and push it to localStorage ----- //
+        array[index].montant = q.target.value;
+        localStorage.setItem("panier", JSON.stringify(array));
+        // ----- Update our innerHtml ----- //
+        let quantityHTML = document.querySelector(`[data-id="${localStorageItem[i].identifiant}"][data-color="${localStorageItem[i].couleur}"] [class="cart__item__content__settings__quantity"]`);
+        console.log(quantityHTML)
+        quantityHTML.firstElementChild.textContent = `Qté : ${q.target.value}`
+      
+        let priceHTML = document.querySelector(`[data-id="${localStorageItem[i].identifiant}"][data-color="${localStorageItem[i].couleur}"] [class="cart__item__content__description"]`)
+        let temp = await getDataById(localStorageItem[i].identifiant);
+        priceHTML.lastChild.textContent = `${q.target.value * temp.price} €`
+        
+        
+        const az = document.getElementById("totalPrice")
+        az.innerText = `${q.target.value * temp.price} €`
+        updatePriceQuantity()
+      }
   }
 }
 
@@ -74,41 +118,82 @@ async function creerElements(){
 /* Actualisation du prix et du nombre d'articles total */ 
 //==================================================================
 
-function actualisation(){  /*(appelé tout en bas) */ 
-  
- let place = document.getElementsByClassName("itemQuantity")
 
- console.log("me donne un tableauHTML,  j'ai mes elements : ", place)
- console.log("n'est pas un tableau?", place[0])
+// ==============================================================
+/* VALIDATION DU FORMULAIRE */ 
 
-for (let i=0 ; i<place.length ; i++){
-   console.log("succes")   /* = Il ne me rends rien */ 
- }
+function validationFormulaire(){
+const formulaire = document.getElementById("order")
 
- // ========================================== 
+const prenom = document.getElementById("firstName")
+const nom = document.getElementById("lastName")
+const addresse = document.getElementById("address")
+const ville = document.getElementById("city")
+const email = document.getElementById("email")
 
- let placeA = document.querySelectorAll(".itemQuantity")
+const error = document.getElementById("firstNameErrorMsg")
+const error2 = document.getElementById("lastNameErrorMsg")
+const error3= document.getElementById("addressErrorMsg")
+const error4 = document.getElementById("cityErrorMsg")
+const error5 = document.getElementById("emailErrorMsg")
+// =================================================
+
+formulaire.addEventListener("click", function(){
+  if (prenom.value == '' || prenom.value == null){
+
+    error.innerText = "Veuillez renseigner votre prénom" 
+  }else{
+    error.remove()
+  }
+})
+
+// ===========
+
+formulaire.addEventListener("click", function(){
+  if (nom.value == '' || nom.value == null){
+
+    error2.innerText = "Veuillez renseigner votre nom de famille" 
+  }else{
+    error2.remove()
+  }
+})
+
+// ==========
+
+formulaire.addEventListener("click", function(){
+  if (addresse.value == '' || addresse.value == null){
+
+    error3.innerText = "Veuillez renseigner votre adresse" 
+  }else{
+    error3.remove()
+  }
+})
+
+// ==========
+
+formulaire.addEventListener("click", function(){
+  if (ville.value == '' || ville.value == null){
+
+    error4.innerText = "Veuillez renseigner votre ville" 
+  }else{
+    error4.remove()
+  }
+})
  
- console.log("me donne une NodeListe vite : ", placeA)
- 
+// ==========
+
+formulaire.addEventListener("click", function(){
+  if (email.value == '' || email.value == null){
+
+    error5.innerText = "Veuillez renseigner votre email" 
+  }else{
+    error5.remove()
+  }
+})
 }
 
 
-
-
-// ==============================================================
-
-
-
-
-
-
-
-
-
+// ==================================================
 creerElements()
 updatePriceQuantity()
-
-
-actualisation() 
-// j'appelle actualisation a la fin, donc mon HTML est deja créer
+validationFormulaire()
