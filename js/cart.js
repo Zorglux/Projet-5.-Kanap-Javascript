47c031282ed0cde70a5bd5c3836496289414b840
Zorglux <tomasdusoulier@gmail.com>
2022-10-17 15:17:51 +0200

    SUPRESSION DU CART FAIIIIIITE.

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-async function getDataById(id) {
+async function apiParId(id) {
   let url = `http://localhost:3000/api/products/${id}`
 
   try {
@@ -10,41 +10,41 @@ async function getDataById(id) {
 }
 // ============================================
 
-function getItemsFromLocalStorage() {
+function rameneLocalStorage() {
   return JSON.parse(localStorage.getItem("panier"));
 }
 // =============================================
 
-async function updatePriceQuantity() {
-  let localStorage = getItemsFromLocalStorage();
-  let price = 0;
-  let quantity = 0;
+async function actualisationQuantitee() {
+  let localStorage = rameneLocalStorage();
+  let prix = 0;
+  let quantitee = 0;
 
   // Calcul du prix du panier
   for (let i = 0; localStorage[i]; i++) {
-    let data = await getDataById(localStorage[i].identifiant);
-    price += parseInt(localStorage[i].montant) * parseInt(data.price);
-    quantity += parseInt(localStorage[i].montant);
+    let donneesApi = await apiParId(localStorage[i].identifiant);
+    prix += parseInt(localStorage[i].montant) * parseInt(donneesApi.price);
+    quantitee += parseInt(localStorage[i].montant);
 
   }
-  // Add Price & Quantity
-  document.getElementById('totalPrice').textContent = `${price}`
-  document.getElementById('totalQuantity').textContent = `${quantity}`
+  // On les ajoutes au dom 
+  document.getElementById('totalPrice').textContent = `${prix}`
+  document.getElementById('totalQuantity').textContent = `${quantitee}`
 }
 // ==========================================================
 
 async function creerElements(){
-  let localStorageItem = getItemsFromLocalStorage()
+  let localObjet = rameneLocalStorage()
 
   // creation des élements
-  for (let i=0; localStorageItem[i]; i++){
-    let donneesApi = await getDataById(localStorageItem[i].identifiant)
+  for (let i=0; localObjet[i]; i++){
+    let donneesApi = await apiParId(localObjet[i].identifiant)
     const placement = document.getElementById("cart__items")
 
     const article = document.createElement("article")
     article.classList.add("cart__item")
-    article.setAttribute("data-id", `${localStorageItem[i].identifiant}`)
-    article.setAttribute("data-color", `${localStorageItem[i].couleur}`)
+    article.setAttribute("data-id", `${localObjet[i].id}`)
+    article.setAttribute("data-color", `${localObjet[i].couleur}`)
     article.innerHTML =
      ` <div class="cart__item__img">
      <img src="${donneesApi.imageUrl}" alt="${donneesApi.altTxt}">
@@ -52,13 +52,13 @@ async function creerElements(){
    <div class="cart__item__content">
      <div class="cart__item__content__description">
        <h2>${donneesApi.name}</h2>
-       <p>${localStorageItem[i].couleur}</p>
+       <p>${localObjet[i].couleur}</p>
        <p>${donneesApi.price}</p>
      </div>
      <div class="cart__item__content__settings">
        <div class="cart__item__content__settings__quantity">
          <p>Qté : </p>
-         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localStorageItem[i].montant}">
+         <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${localObjet[i].montant}">
        </div>
        <div class="cart__item__content__settings__delete">
          <p class="deleteItem">Supprimer</p>
@@ -68,55 +68,72 @@ async function creerElements(){
 
     placement.appendChild(article)
 
-    let qtyInput = article.getElementsByClassName('itemQuantity');
-      console.log("n'est pas un tableau?", qtyInput[0])
-      qtyInput[0].addEventListener('change', (e) => {
-        updateQtyLocalStorage(e, localStorageItem[i].id, localStorageItem[i].color);
-      });
-      
-      /**
-       * @param  {HTMLElement} q
-       * @param  {string} id
-       * @param  {string} color
-       * update localStorage when a quantity change is triggered
-       */
-      async function updateQtyLocalStorage(q, id, color) {
-        let array = getItemsFromLocalStorage();
-        let idArray = [];
-        let index;
-        // error handling
-        if (q.target.value <= 0)
-          return -1;
-        // ----- get the index of our item in our array ----- //
-        for (let i = 0; array[i]; i++) {
-          if (array[i].id == id && array[i].color == color)
-            index = i;
-          idArray.push(array[i].id);
-        }
-      
-        // ----- change the value of the quantity and push it to localStorage ----- //
-        array[index].montant = q.target.value;
-        localStorage.setItem("panier", JSON.stringify(array));
-        // ----- Update our innerHtml ----- //
-        let quantityHTML = document.querySelector(`[data-id="${localStorageItem[i].identifiant}"][data-color="${localStorageItem[i].couleur}"] [class="cart__item__content__settings__quantity"]`);
-        console.log(quantityHTML)
-        quantityHTML.firstElementChild.textContent = `Qté : ${q.target.value}`
-      
-        let priceHTML = document.querySelector(`[data-id="${localStorageItem[i].identifiant}"][data-color="${localStorageItem[i].couleur}"] [class="cart__item__content__description"]`)
-        let temp = await getDataById(localStorageItem[i].identifiant);
-        priceHTML.lastChild.textContent = `${q.target.value * temp.price} €`
-        
-        
-        const az = document.getElementById("totalPrice")
-        az.innerText = `${q.target.value * temp.price} €`
-        updatePriceQuantity()
+    // ===================================================================
+/* Calcul du prix et du nombre d'articles total */ 
+//==================================================================
+    let placementInput = article.getElementsByClassName('itemQuantity');
+
+    placementInput[0].addEventListener('change', (e) => {
+      actualisationLocalStorage(e, localObjet[i].id, localObjet[i].color);
+    });
+    
+    
+
+    async function actualisationLocalStorage(q, id, color) {
+      let local = rameneLocalStorage();
+      let idLocal = [];
+      let index;
+      // error handling
+      if (q.target.value <= 0)
+        return -1;
+      // ----- On mets l'index dans notre tableau ----- //
+      for (let i = 0; local[i]; i++) {
+        if (local[i].id == id && local[i].color == color)
+          index = i;
+        idLocal.push(local[i].id);
       }
-  }
+    
+      // ----- change the value of the quantity and push it to localStorage ----- //
+      local[index].montant = q.target.value;
+      localStorage.setItem("panier", JSON.stringify(local));
+
+      // ----- On remets a jour le total ----- //
+
+              actualisationQuantitee()
+            }
+
+            function suppression(){
+              let boutonSupprimer =  article.getElementsByClassName("deleteItem")
+              let idParBouton = boutonSupprimer[0].closest("article").getAttribute("data-id")
+              let articleDuBouton = boutonSupprimer[0].closest("article")
+              
+              boutonSupprimer[0].addEventListener("click", function(){
+                let local = rameneLocalStorage();
+               
+                let localSupprime =  local.filter(function(local) {
+                  return local.id != idParBouton ;
+                });
+                localStorage.setItem("panier", JSON.stringify(localSupprime));
+                
+               actualisationQuantitee()
+               articleDuBouton.remove()
+              
+              })
+            }
+            suppression()
+            
+           
+    }
 }
 
-// ===================================================================
-/* Actualisation du prix et du nombre d'articles total */ 
-//==================================================================
+
+
+
+
+
+
+
+
 
 
 // ==============================================================
@@ -194,6 +211,11 @@ formulaire.addEventListener("click", function(){
 
 
 // ==================================================
+
+
+
+
+
 creerElements()
-updatePriceQuantity()
+actualisationQuantitee()
 validationFormulaire()
